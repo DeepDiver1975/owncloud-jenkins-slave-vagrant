0cfcf622d872059fc90e012cd206a1b6f8424012
Thomas Müller <thomas.mueller@tmit.eu>
2014-09-12 23:45:16 +0200

    mysql and pgsql support added

--- setup.sh
+++ setup.sh
@@ -8,6 +8,8 @@
 
 set -e
 
+killall java || true
+
 # read config
 if [ -f ./jenkins.config ]; then
   source ./jenkins.config
@@ -35,12 +37,41 @@ sudo apt-get -y install default-jre-headless git ant phpunit
 sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password your_password'
 sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password your_password'
 sudo apt-get -y install mysql-server postgresql
-sudo apt-get -y install php5 php5-sqlite php5-pgsql php5-mysqlnd php5-gd php5-intl php5-curl php5-ldap
+sudo apt-get -y install php5 php5-sqlite php5-pgsql php5-mysqlnd php5-gd php5-intl php5-curl php5-ldap php5-dev
 sudo apt-get -y install smbclient
 
 # setup mysql
 mysql -u root -pyour_password -e "CREATE DATABASE IF NOT EXISTS oc_autotest0; grant all on oc_autotest0.* to 'oc_autotest0'@'localhost' IDENTIFIED BY 'owncloud';"
 mysql -u root -pyour_password -e "CREATE DATABASE IF NOT EXISTS oc_autotest1; grant all on oc_autotest1.* to 'oc_autotest1'@'localhost' IDENTIFIED BY 'owncloud';"
+mysql -u root -pyour_password -e "CREATE DATABASE IF NOT EXISTS oc_autotest2; grant all on oc_autotest2.* to 'oc_autotest2'@'localhost' IDENTIFIED BY 'owncloud';"
+mysql -u root -pyour_password -e "CREATE DATABASE IF NOT EXISTS oc_autotest3; grant all on oc_autotest3.* to 'oc_autotest3'@'localhost' IDENTIFIED BY 'owncloud';"
+
+# setup pgsql
+sudo su - postgres <<'EOF'
+psql -c "DROP DATABASE IF EXISTS oc_autotest0; DROP ROLE IF EXISTS oc_autotest0; CREATE USER oc_autotest0 WITH SUPERUSER PASSWORD 'owncloud';"
+psql -c "DROP DATABASE IF EXISTS oc_autotest1; DROP ROLE IF EXISTS oc_autotest1; CREATE USER oc_autotest1 WITH SUPERUSER PASSWORD 'owncloud';"
+psql -c "DROP DATABASE IF EXISTS oc_autotest2; DROP ROLE IF EXISTS oc_autotest2; CREATE USER oc_autotest2 WITH SUPERUSER PASSWORD 'owncloud';"
+psql -c "DROP DATABASE IF EXISTS oc_autotest3; DROP ROLE IF EXISTS oc_autotest3; CREATE USER oc_autotest3 WITH SUPERUSER PASSWORD 'owncloud';"
+EOF
+sudo bash -c 'cat > /etc/postgresql/9.4/main/pg_hba.conf <<DELIM
+# Database administrative login by Unix domain socket
+local   all             postgres                                peer
+# all local connections are trusted
+local	all		all					trust
+# "local" is for Unix domain socket connections only
+local   all             all                                     peer
+# IPv4 local connections:
+host    all             all             127.0.0.1/32            md5
+# IPv6 local connections:
+host    all             all             ::1/128                 md5
+DELIM'
+
+sudo service postgresql restart
+
+#setup oracle
+sudo dpkg --add-architecture i386
+sudo apt-get update
+#sudo bash ~/setup-oracle.sh
 
 # install nodejs
 sudo apt-get -y install curl
@@ -56,5 +87,6 @@ rm -rf slave.jar
 wget --no-check-certificate https://ci.owncloud.org/jnlpJars/slave.jar
 
 #start the slave
-java -jar slave.jar -noCertificateCheck -jnlpUrl https://ci.owncloud.org/computer/$SLAVE_NAME/slave-agent.jnlp -secret $SLAVE_SECRET
+echo "Starting Jenkins slave $SLAVE_NAME"
+java -jar slave.jar -noCertificateCheck -jnlpUrl https://ci.owncloud.org/computer/$SLAVE_NAME/slave-agent.jnlp -secret $SLAVE_SECRET &
 
