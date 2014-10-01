4da14dd266c0b7e5c56db591bf2d65c55e9813b3
Thomas Müller <thomas.mueller@tmit.eu>
2014-10-01 19:46:19 +0200

    support up to 8 executors

--- setup.sh
+++ setup.sh
@@ -33,6 +33,9 @@ sudo apt-get update && sudo apt-get -y upgrade
 # install jenkins dependencies
 sudo apt-get -y install default-jre-headless git ant phpunit curl
 echo -e "Host github.com\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
+#wget https://phar.phpunit.de/phpunit.phar
+#chmod +x phpunit.phar
+#sudo mv phpunit.phar /usr/local/bin/phpunit
 
 # install owncloud dependencies
 sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password your_password'
@@ -46,6 +49,10 @@ mysql -u root -pyour_password -e "CREATE DATABASE IF NOT EXISTS oc_autotest0; gr
 mysql -u root -pyour_password -e "CREATE DATABASE IF NOT EXISTS oc_autotest1; grant all on oc_autotest1.* to 'oc_autotest1'@'localhost' IDENTIFIED BY 'owncloud';"
 mysql -u root -pyour_password -e "CREATE DATABASE IF NOT EXISTS oc_autotest2; grant all on oc_autotest2.* to 'oc_autotest2'@'localhost' IDENTIFIED BY 'owncloud';"
 mysql -u root -pyour_password -e "CREATE DATABASE IF NOT EXISTS oc_autotest3; grant all on oc_autotest3.* to 'oc_autotest3'@'localhost' IDENTIFIED BY 'owncloud';"
+mysql -u root -pyour_password -e "CREATE DATABASE IF NOT EXISTS oc_autotest4; grant all on oc_autotest4.* to 'oc_autotest4'@'localhost' IDENTIFIED BY 'owncloud';"
+mysql -u root -pyour_password -e "CREATE DATABASE IF NOT EXISTS oc_autotest5; grant all on oc_autotest5.* to 'oc_autotest5'@'localhost' IDENTIFIED BY 'owncloud';"
+mysql -u root -pyour_password -e "CREATE DATABASE IF NOT EXISTS oc_autotest6; grant all on oc_autotest6.* to 'oc_autotest6'@'localhost' IDENTIFIED BY 'owncloud';"
+mysql -u root -pyour_password -e "CREATE DATABASE IF NOT EXISTS oc_autotest7; grant all on oc_autotest7.* to 'oc_autotest7'@'localhost' IDENTIFIED BY 'owncloud';"
 
 # setup pgsql
 sudo su - postgres <<'EOF'
@@ -53,10 +60,18 @@ psql -c "DROP DATABASE IF EXISTS oc_autotest0;"
 psql -c "DROP DATABASE IF EXISTS oc_autotest1;"
 psql -c "DROP DATABASE IF EXISTS oc_autotest2;"
 psql -c "DROP DATABASE IF EXISTS oc_autotest3;"
+psql -c "DROP DATABASE IF EXISTS oc_autotest4;"
+psql -c "DROP DATABASE IF EXISTS oc_autotest5;"
+psql -c "DROP DATABASE IF EXISTS oc_autotest6;"
+psql -c "DROP DATABASE IF EXISTS oc_autotest7;"
 psql -c "DROP ROLE IF EXISTS oc_autotest0; CREATE USER oc_autotest0 WITH SUPERUSER PASSWORD 'owncloud';"
 psql -c "DROP ROLE IF EXISTS oc_autotest1; CREATE USER oc_autotest1 WITH SUPERUSER PASSWORD 'owncloud';"
 psql -c "DROP ROLE IF EXISTS oc_autotest2; CREATE USER oc_autotest2 WITH SUPERUSER PASSWORD 'owncloud';"
 psql -c "DROP ROLE IF EXISTS oc_autotest3; CREATE USER oc_autotest3 WITH SUPERUSER PASSWORD 'owncloud';"
+psql -c "DROP ROLE IF EXISTS oc_autotest4; CREATE USER oc_autotest4 WITH SUPERUSER PASSWORD 'owncloud';"
+psql -c "DROP ROLE IF EXISTS oc_autotest5; CREATE USER oc_autotest5 WITH SUPERUSER PASSWORD 'owncloud';"
+psql -c "DROP ROLE IF EXISTS oc_autotest6; CREATE USER oc_autotest6 WITH SUPERUSER PASSWORD 'owncloud';"
+psql -c "DROP ROLE IF EXISTS oc_autotest7; CREATE USER oc_autotest7 WITH SUPERUSER PASSWORD 'owncloud';"
 EOF
 sudo bash -c 'cat > /etc/postgresql/9.4/main/pg_hba.conf <<DELIM
 # Database administrative login by Unix domain socket
@@ -92,7 +107,7 @@ fi
 
 # install php 5.3
 if [ ! -f  /home/vagrant/.phpenv/bin/phpenv ]; then
-  sudo apt-get install libxml2-dev re2c libmcrypt-dev libcurl3-openssl-dev bison flex libjpeg62-dev libpng-dev libtidy-dev libxslt-dev libreadline-dev libldap2-dev libpq-dev
+  sudo apt-get install libxml2-dev re2c libmcrypt-dev libcurl3-openssl-dev bison flex libjpeg62-dev libpng-dev libtidy-dev libxslt-dev libreadline-dev libldap2-dev libpq-dev libbz2-dev libicu-dev libircclient-dev 
   export PHPENV_ROOT=/home/vagrant/.phpenv
   rm -rf phpenv-install.sh
   wget https://raw.github.com/CHH/phpenv/master/bin/phpenv-install.sh
@@ -103,8 +118,8 @@ if [ ! -f  /home/vagrant/.phpenv/bin/phpenv ]; then
   echo 'eval "$(phpenv init -)"' >> /home/vagrant/.bashrc
 
   export PATH="/home/vagrant/.phpenv/bin:$PATH"
-  export PHP_BUILD_CONFIGURE_OPTS="--with-libdir=/lib/x86_64-linux-gnu --with-ldap --with-pgsql --with-pear"
-
+  export PHP_BUILD_CONFIGURE_OPTS="--with-libdir=/lib/x86_64-linux-gnu --with-ldap --with-pgsql --with-mysql --with-pear --with-bz2 --enable-hash --with-mhash=yes --enable-intl --with-pdo-pgsql --with-pdo-mysql --with-readline"
+  export LDFLAGS="-lstdc++"
 # export PHP_BUILD_CONFIGURE_OPTS="--enable-fileinfo --enable-hash --enable-json --enable-bcmath --with-bz2 --enable-ctype --with-iconv --with-gettext --with-pcre-regex --enable-phar --enable-simplexml --enable-dom --with-libxml-dir=/usr --enable-tokenizer --with-mhash=yes --with-gd --enable-calendar --enable-cli --enable-cgi --enable-gd-native-ttf --enable-mbregex --enable-wddx --enable-zend-multibyte --with-iodbc --with-ldap-sasl --with-ldap --with-pgsql --with-pear"
   php_versions=( 5.3.28 5.4.32 5.5.16 5.6.0 )
   for php_version in ${php_versions[@]} do
@@ -112,8 +127,8 @@ if [ ! -f  /home/vagrant/.phpenv/bin/phpenv ]; then
     echo "Install PHP $php_version"
     phpenv install $php_version
     # install oci
+    export ORACLE_HOME=/usr/lib/oracle/11.2/client64
     phpenv local $php_version
-    printf "/usr/lib/oracle/11.2/client64\n" | pecl install oci8
     echo "extension = oci8.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
 
   done
